2b00ce8902a618710031fc9cd529ddda475c5090
Arthur Nicholas <94144917+arthurnich0las@users.noreply.github.com>
2023-05-09 11:20:27 -0300

    adicionando comentarios

--- helpers.py
+++ helpers.py
@@ -4,15 +4,15 @@ def get_valid_ip():
     while True:
         try:
             ip = input('IP do servidor: ')
-            ip_address(ip)
-            return ip
+            ip_address(ip)  # verificando se o ip é valido, caso não, sobe o erro "ValueError"
+            return ip       # retorna o ip se tiver tudo certo
         except ValueError:
-            print('Endereço IP inválido.\nTente novamente.')
+            print('Endereço IP inválido.\nTente novamente.')    # tenta novamente caso esteja errado
 
 def get_valid_port():
     while True:
         try:
-            port = int(input('Porta: '))
-            return port
+            port = int(input('Porta: '))    # pede a porta para conexão
+            return port                     # retorna a porta caso seja um numero inteiro, do contrário sobe o erro ValueError
         except ValueError: 
             print('A porta tem que ser um número inteiro.\nTente novamente.')
